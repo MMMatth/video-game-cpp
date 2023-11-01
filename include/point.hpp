9dd3fe71b964ee231bf0cdabccb6a1239eaec2ff
MMMatth <mat.loup04@gmail.com>
2023-11-01 11:00:38 +0100

    I'm putting back point

--- include/point.hpp
+++ include/point.hpp
@@ -11,12 +11,19 @@
  * \class Point
  * \brief class représentant un point dans un espace 2D.
  */
-#ifndef POINT_HPP
-#define POINT_HPP
+class Point {
+public:
+  Point() : m_x(0), m_y(0) {}
+  Point(int x, int y) : m_x(x), m_y(y) {}
+  int getX() const { return m_x; }
+  int getY() const { return m_y; }
+  void setX(int x) { m_x = x; }
+  void setY(int y) { m_y = y; }
+  // std::string toString();
 
-<<<<<<< HEAD int x = 1;
-=======
-int x = 2;
->>>>>>> other-branch
+private:
+  int m_x; /*!< Coordonnée horizontale du point. */
+  int m_y; /*!< Coordonnée verticale du point. */
+};
 
 #endif
