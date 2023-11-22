8c7e198d61d10e0e64f44a75215cf747413ce133
Cisse PapaElHadjiG <papa-el-hadji-g.cisse2@etu.univ-lorraine.fr>
2023-11-22 22:13:22 +0100

    adding comments on the files : -block.hpp -cam.hpp -character.hpp

--- include/block.hpp
+++ include/block.hpp
@@ -1,5 +1,3 @@
-
-
 #ifndef BLOCK_HPP
 #define BLOCK_HPP
 
@@ -13,31 +11,80 @@
 
 using namespace std;
 
+/**
+ * @class Block
+ * @brief Represents a block in the game world.
+ *
+ * This class inherits from the Item class and adds properties specific to blocks,
+ * such as solidity and time to break.
+*/
 class Block : public Item {
 public:
-  /* constructor */
+
+  /**
+   * @brief Default constructor for Block class.
+  */
   Block();
+
+  /**
+   * @brief Parameterized constructor for Block class.
+   * @param id The unique identifier for the block.
+   * @param name The name of the block.
+   * @param isSolid Indicates whether the block is solid.
+   * @param time_to_break The time it takes to break the block (default is 2000).
+   * @param is_stackable Indicates whether the block is stackable (default is true).
+   * @param amount The amount of the block (default is 1).
+  */
   Block(string id, string name, bool isSolid, int time_to_break = 2000,
         bool is_stackable = true, int amount = 1);
 
-  /* getters */
+  /**
+   * @brief Getter function for checking if the block is solid.
+   * @return True if the block is solid, false otherwise.
+  */
   bool isSolid() const { return m_isSolid; }
+
+  /**
+   * @brief Getter function for retrieving the unique identifier of the block.
+   * @return The unique identifier of the block.
+  */
   string getId() { return m_id; }
+
+  /**
+   * @brief Getter function for retrieving the time it takes to break the block.
+   * @return The time it takes to break the block.
+  */
   int getTimeToBreak() { return m_time_to_break; }
 
-  /* setters */
+  /**
+   * @brief Setter function for setting the time it takes to break the block.
+   * @param hardness The time it takes to break the block.
+  */
   void setTimeToBreak(int hardness) { m_time_to_break = hardness; }
+
+  /**
+   * @brief Setter function for setting whether the block is solid.
+   * @param isSolid True if the block is solid, false otherwise.
+  */
   void setSolid(bool isSolid) { m_isSolid = isSolid; }
 
-  /* other */
+  /**
+   * @brief Convert the block information to a string format.
+   * @return A string representation of the block.
+  */
   string toString() const;
 
 private:
-  bool m_isSolid;
-  string m_id;
-  int m_time_to_break;
+  bool m_isSolid;  /*Indicates whether the block is solid.*/
+  string m_id;  /*The unique identifier of the block.*/
+  int m_time_to_break; /*The time it takes to break the block.*/
 };
 
+/**
+ * @brief Map of predefined blocks with their unique identifiers.
+ * This map contains a set of predefined blocks with their unique identifiers
+ * and corresponding Block objects.
+ */
 static map<string, Block> blockMap = {
     {"AIR", Block("0", "AIR", false, false)},
     {"GRASS", Block("1", "GRASS", true, 100)},
--- include/cam.hpp
+++ include/cam.hpp
@@ -12,35 +12,116 @@
 using namespace sf;
 using namespace std;
 
+/**
+ * @class Cam
+ * @brief Represents the camera in the game world.
+ * The camera is responsible for controlling the view and position within the game.
+ */
 class Cam {
 private:
-  bool m_save;
-  Coord m_pos;
-  int m_width = CAM_WIDTH;
-  int m_height = CAM_HEIGHT;
+  bool m_save; /*Indicates whether the camera state should be saved.*/
+  Coord m_pos; /*The position of the camera.*/
+  int m_width = CAM_WIDTH; /*The width of the camera view.*/
+  int m_height = CAM_HEIGHT; /*The height of the camera view.*/
 
 public:
+  /**
+   * @brief Default constructor for the Cam class.
+  */
   Cam();
+
+  /**
+   * @brief Parameterized constructor for the Cam class.
+   * @param x The initial x-coordinate of the camera.
+   * @param y The initial y-coordinate of the camera.
+  */
   Cam(int x, int y);
+
+  /**
+   * @brief Constructor for the Cam class that loads camera state from a CSV file.
+   * @param csvPath The path to the CSV file containing camera state information.
+  */
   Cam(string csvPath);
+
+  /**
+   * @brief Loads camera state from a CSV file.
+   * @param csvPath The path to the CSV file containing camera state information.
+   * @return True if the loading is successful, false otherwise.
+  */
   bool loadFromCSV(string csvPath);
-  /* update */
+
+  /**
+   * @brief Updates the camera position based on the character's position and the window dimensions.
+   * @param charX The x-coordinate of the character.
+   * @param charY The y-coordinate of the character.
+   * @param charW The width of the character.
+   * @param charH The height of the character.
+   * @param mapWidth The width of the game map.
+   * @param mapHeight The height of the game map.
+   * @param window The RenderWindow object representing the game window.
+  */
   void update(int charX, int charY, int charW, int charH, int mapWidth,
               int mapHeight, RenderWindow &window);
-  /* getters */
+  
+  /**
+   * @brief Getter function for retrieving the x-coordinate of the camera.
+   * @return The x-coordinate of the camera.
+  */
   int getX() { return m_pos.getX(); }
+
+  /**
+   * @brief Getter function for retrieving the y-coordinate of the camera.
+   * @return The y-coordinate of the camera.
+  */
   int getY() { return m_pos.getY(); }
+
+  /**
+   * @brief Getter function for retrieving the position of the camera.
+   * @return The position of the camera as a Coord object.
+  */
   Coord getPos() { return m_pos; }
+
+  /**
+   * @brief Getter function for retrieving the width of the camera view.
+   * @return The width of the camera view.
+   */
   int getWidth() { return m_width; }
+
+  /**
+   * @brief Getter function for retrieving the height of the camera view.
+   * @return The height of the camera view.
+  */
   int getHeight() { return m_height; }
 
-  /* setters */
+  /**
+   * @brief Setter function for setting the x-coordinate of the camera.
+   * @param x The x-coordinate of the camera.
+  */
   void setX(int x) { m_pos.setX(x); }
+
+  /**
+   * @brief Setter function for setting the y-coordinate of the camera.
+   * @param y The y-coordinate of the camera.
+  */
   void setY(int y) { m_pos.setY(y); }
+
+  /**
+   * @brief Setter function for setting the position of the camera.
+   * @param pos The position of the camera as a Coord object.
+  */
   void setPos(Coord pos) { m_pos = pos; }
 
-  /* other */
+  /**
+   * @brief Resets the camera to a specified position.
+   * @param x The x-coordinate of the new camera position.
+   * @param y The y-coordinate of the new camera position.
+   */
   void reset(int x, int y);
+
+  /**
+   * @brief Saves the camera state to a CSV file.
+   * @param csvPath The path to the CSV file where the camera state will be saved.
+  */
   void save(string csvPath);
 };
 
