ce42a568d9729e41e9736d33bfd0c859e846ca8e
Cisse PapaElHadjiG <papa-el-hadji-g.cisse2@etu.univ-lorraine.fr>
2023-11-27 00:56:59 +0100

    added comments on the tool class

--- include/item/tool.hpp
+++ include/item/tool.hpp
@@ -8,17 +8,39 @@
 
 using namespace std;
 
+/**
+ * @class Tool
+ * @brief Represents a tool item in the game.
+ *
+ * This class is derived from the Item class and represents tools that can be used
+ * for specific actions in the game.
+*/
 class Tool : public Item {
 private:
 public:
   /* constructor */
+
+  /**
+   * Parameterized constructor for Tool.
+   * @param name The name of the tool.
+   * @param is_stackable Whether the tool is stackable (default is false).
+   * @param amount The amount of the tool (default is 1).
+  */
   Tool(string name, bool is_stackable = false, int amount = 1);
+
+  /** Default constructor for Tool. */
   Tool();
   /* destructor */
   // ~Tool(){};
-  /* getters */
+
+
 };
 
+/**
+ * @brief A map that associates tool names with Tool objects.
+ *
+ * This map is used to store predefined tool objects for easy access.
+*/
 static map<string, Tool> toolMap = {{"IRON_PICKAXE", Tool("IRON_PICKAXE")},
                                     {"IRON_AXE", Tool("IRON_AXE")}};
 
