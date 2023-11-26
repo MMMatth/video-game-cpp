d4b640763fbb336592765c23c8d7c9fc1e144b97
Cisse PapaElHadjiG <papa-el-hadji-g.cisse2@etu.univ-lorraine.fr>
2023-11-27 00:32:51 +0100

    added comments on the walking_monster class

--- include/entity/walking_monster.hpp
+++ include/entity/walking_monster.hpp
@@ -6,16 +6,41 @@
 using namespace std;
 using namespace sf;
 
+/**
+ * @class WalkingMonster
+ * @brief Represents a walking type of monster in the game.
+ *
+ * This class inherits from the Monster class and provides specific
+ * implementation for a walking type of monster, including walking behavior.
+*/
 class WalkingMonster : public Monster {
 private:
-  int m_timeJump;
-  int m_jumpHeight;
+  int m_timeJump; // Elapsed time during the jump
+  int m_jumpHeight; // The limit of time jump
 
 public:
+  /**
+   * @brief Parameterized constructor for WalkingMonster.
+   * @param x The initial x-coordinate of the monster.
+   * @param y The initial y-coordinate of the monster.
+   * @param width The width of the monster.
+   * @param height The height of the monster.
+   * @param speed The movement speed of the monster.
+   * @param life The life points of the monster.
+   * @param jumpHeight The limit of time for jumping.
+  */
   WalkingMonster(int x, int y, int width, int height, int speed, int life,
                  int jumpHeight)
       : Monster(x, y, width, height, speed, life), m_jumpHeight(jumpHeight) {}
+  
+  /**
+   * @brief Updates the state and behavior of the walking monster.
+   * @details This function is called during each game update to handle the
+   * walking monster's behavior.
+  */
   void update() override;
+  
+  // Destructor (if needed)
   //   ~WalkingMonster();
 };
 
