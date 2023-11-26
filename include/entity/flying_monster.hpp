4937bb49cd79e6a491dcfbcdbcbc724cfd58a9da
Cisse PapaElHadjiG <papa-el-hadji-g.cisse2@etu.univ-lorraine.fr>
2023-11-27 00:25:01 +0100

    added comments on the flying_monster class

--- include/entity/flying_monster.hpp
+++ include/entity/flying_monster.hpp
@@ -6,14 +6,43 @@
 using namespace std;
 using namespace sf;
 
+/**
+ * @class FlyingMonster
+ * @brief Represents a flying monster entity in the game.
+ *
+ * This class inherits from the Monster class and implements the 'update'
+ * function to define the specific behavior of a flying monster.
+*/
 class FlyingMonster : public Monster {
-private:
-  /* data */
+
 public:
+  /**
+   * @brief Default constructor for FlyingMonster.
+   * @details Initializes the FlyingMonster with default values.
+  */
   FlyingMonster() : Monster(0, 0, 0, 0, 0, 0) {}
+
+  /**
+   * @brief Parameterized constructor for FlyingMonster.
+   * @param x The initial x-coordinate of the flying monster.
+   * @param y The initial y-coordinate of the flying monster.
+   * @param width The width of the flying monster.
+   * @param height The height of the flying monster.
+   * @param speed The movement speed of the flying monster.
+   * @param life The initial life points of the flying monster.
+  */
   FlyingMonster(int x, int y, int width, int height, int speed, int life)
       : Monster(x, y, width, height, speed, life) {}
+      
+  /**
+   * @brief Function to update the state and behavior of the flying monster.
+   * @details This function is called to update the flying monster's position,
+   * animation, or any other specific behavior.
+  */
   void update() override;
+
+private:
+  /* data */
 };
 
 #endif /* FLYING_MONSTER_HPP */
