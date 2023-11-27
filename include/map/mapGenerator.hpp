5ee2babbffb729b95f79d25eb21412b90acbcd8e
Cisse PapaElHadjiG <papa-el-hadji-g.cisse2@etu.univ-lorraine.fr>
2023-11-27 01:08:02 +0100

    added comment on the mapGenerator class

--- include/map/mapGenerator.hpp
+++ include/map/mapGenerator.hpp
@@ -9,12 +9,23 @@
 
 using namespace std;
 
+/**
+ * @struct altitude
+ * @brief Represents the altitude range with maximum, minimum, and size.
+*/
 struct altitude {
-  int max;
-  int min;  // position la plus basse
-  int size; // altitude max - altitude min + 1
+  int max;  /**< The maximum altitude. */
+  int min;  /**< The minimum altitude. */
+  int size; /**< The size of the altitude range (max - min + 1). */
 };
 
+/**
+ * @class Createmap
+ * @brief Represents a map generator.
+ *
+ * This class is responsible for generating game maps with various features such
+ * as terrain, caves, trees, and more.
+*/
 class Createmap {
 private:
   int m_seed;                   // seed for the random
@@ -25,6 +36,10 @@ private:
   int m_underground_size;       // size of the underground
 
 public:
+  /**
+   * Parameterized constructor for Createmap.
+   * @param width The width of the map.
+  */
   Createmap(int width);
 
   /**
@@ -34,26 +49,60 @@ public:
    */
   void setCurveAltitude();
 
+   /**
+   * Add stone blocks to the map based on the altitude curve.
+   */
   void addStone();
 
+  /**
+   * Add cave-like structures to the map.
+   */
   void addCave();
 
+  /**
+   * Add grass blocks to the map based on the altitude curve.
+   */
   void addGrass();
 
+  /**
+   * Add dirt blocks to the map based on the altitude curve.
+   */
   void addDirt();
 
+  /**
+   * Add sky blocks to the top layer of the map.
+   */
   void addSky();
 
+  /**
+   * Add tree structures to the map.
+   */
   void addTree();
 
+  /**
+   * Add leaf blocks to the map for trees.
+   */
   void addLeaf();
 
+  /**
+   * Add flower blocks to the map.
+   */
   void addFlower();
 
+  /**
+   * Set the entire map based on the generated features.
+   */
   void setMap();
 
+  /**
+   * Save the generated map to a file.
+   * @param filename The name of the file where the map will be saved.
+   */
   void saveinfile(string filename);
 
+  /**
+   * Generate the map with various features.
+   */
   void generate();
 };
 
