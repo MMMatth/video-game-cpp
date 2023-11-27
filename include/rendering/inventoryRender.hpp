b61bff93d4eecb68b2b52399884116eaade629c2
Cisse PapaElHadjiG <papa-el-hadji-g.cisse2@etu.univ-lorraine.fr>
2023-11-27 01:13:17 +0100

    added comments on the inventoryRender class

--- include/rendering/inventoryRender.hpp
+++ include/rendering/inventoryRender.hpp
@@ -11,24 +11,78 @@
 using namespace sf;
 using namespace std;
 
+/**
+ * @class InventoryRenderer
+ * @brief Responsible for rendering the inventory in the game.
+ *
+ * This class handles the rendering of the player's inventory, including the
+ * lower bar, inventory grid, and selected item.
+ */
 class InventoryRenderer {
 private:
-  Inventory &m_inventory;
+  Inventory &m_inventory; /**< Reference to the player's inventory. */
 
+  /**
+   * Draw an inventory tile at a specific position on the window.
+   * @param window Reference to the SFML RenderWindow.
+   * @param sprites Map of sprites containing the inventory tile images.
+   * @param x X-coordinate of the tile on the window.
+   * @param y Y-coordinate of the tile on the window.
+   * @param row Row index of the tile in the inventory grid.
+   * @param column Column index of the tile in the inventory grid.
+   */
   void drawTile(sf::RenderWindow &window, unordered_map<string, Sprite> sprites,
                 int x, int y, int row, int column);
+
+  /**
+   * Draw the lower bar of the inventory.
+   * @param window Reference to the SFML RenderWindow.
+   * @param sprites Map of sprites containing the lower bar images.
+   * @param persoX X-coordinate of the player.
+   * @param persoY Y-coordinate of the player.
+   */
   void drawLowerBar(sf::RenderWindow &window,
                     unordered_map<string, Sprite> sprites, int persoX,
                     int persoY);
+
+  /**
+   * Draw the entire inventory grid.
+   * @param window Reference to the SFML RenderWindow.
+   * @param sprites Map of sprites containing the inventory tile images.
+   * @param camX X-coordinate of the camera.
+   * @param camY Y-coordinate of the camera.
+   */
   void drawInv(sf::RenderWindow &window, unordered_map<string, Sprite> sprites,
                int camX, int camY);
+
+  /**
+   * Draw the selected item in the inventory.
+   * @param window Reference to the SFML RenderWindow.
+   * @param sprites Map of sprites containing the inventory tile images.
+   * @param camX X-coordinate of the camera.
+   * @param camY Y-coordinate of the camera.
+   * @param mouseX X-coordinate of the mouse.
+   * @param mouseY Y-coordinate of the mouse.
+   */
   void drawSelectedItem(sf::RenderWindow &window,
                         unordered_map<string, Sprite> sprites, int camX,
                         int camY, int mouseX, int mouseY);
 
 public:
+  /**
+   * Parameterized constructor for InventoryRenderer.
+   * @param inv Reference to the player's inventory.
+   */
   InventoryRenderer(Inventory &inv) : m_inventory(inv) {}
 
+  /**
+   * Render the entire inventory on the window.
+   * @param window Reference to the SFML RenderWindow.
+   * @param sprites Map of sprites containing the inventory tile images.
+   * @param cam Reference to the camera.
+   * @param mouseX X-coordinate of the mouse.
+   * @param mouseY Y-coordinate of the mouse.
+   */
   void render(sf::RenderWindow &window,
               std::unordered_map<std::string, sf::Sprite> sprites, Cam &cam,
               int mouseX, int mouseY);
