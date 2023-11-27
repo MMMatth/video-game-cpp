80e0858174b26998dc1d8be03cbf13b752280bd1
Cisse PapaElHadjiG <papa-el-hadji-g.cisse2@etu.univ-lorraine.fr>
2023-11-27 01:19:05 +0100

    added comments on the menuPause class

--- include/ui/menuPause.hpp
+++ include/ui/menuPause.hpp
@@ -1,4 +1,3 @@
-
 #ifndef MENUPAUSE_HPP
 #define MENUPAUSE_HPP
 
@@ -13,30 +12,49 @@
 using namespace std;
 using namespace sf;
 
+/**
+ * @class MenuPause
+ * @brief Represents the pause menu in the game.
+ *
+ * The MenuPause class handles the functionality of the pause menu,
+ * including rendering, event handling, and sound settings.
+ */
 class MenuPause {
 private:
-  bool m_pause;
-  Texture pauseTexture;
-  Sprite pauseSprite;
-  Sound &m_sound;
-  SoundSettings &m_soundSettings;
-  int m_clickOnOff;
+  bool m_pause; /**< Flag indicating whether the game is paused. */
+  Texture pauseTexture; /**< SFML texture for the pause menu background. */
+  Sprite pauseSprite; /**< SFML sprite for the pause menu background. */
+  Sound &m_sound; /**< Reference to the SFML Sound object for sound settings. */
+  SoundSettings &m_soundSettings; /**< Reference to the sound settings. */
+  int m_clickOnOff; /**< Counter to prevent rapid menu interactions. */
 
 public:
-  MenuPause(SoundSettings &SoundSettings, Sound &sound)
-      : m_pause(false), m_clickOnOff(2), m_soundSettings(SoundSettings),
+  /**
+   * Parameterized constructor for MenuPause.
+   * @param soundSettings Reference to the SoundSettings object.
+   * @param sound Reference to the SFML Sound object.
+   */
+  MenuPause(SoundSettings &soundSettings, Sound &sound)
+      : m_pause(false), m_clickOnOff(2), m_soundSettings(soundSettings),
         m_sound(sound) {
     Error(!pauseTexture.loadFromFile(IMG_PAUSE_ON),
           "Error loading IMG_PAUSE_ON texture");
     pauseSprite.setTexture(pauseTexture);
   }
 
+  /**
+   * Handle the pause action.
+   */
   void handlePause() {
     m_pause = true;
     m_soundSettings.setVolume(0);
     m_sound.setVolume(m_soundSettings.getVolume());
   }
 
+  /**
+   * Handle mouse button press events for the pause menu.
+   * @param event SFML MouseButtonEvent.
+   */
   void handleMouseButtonPressed(sf::Event::MouseButtonEvent &event) {
     if (event.button == Mouse::Left) {
       if (m_pause) {
@@ -66,6 +84,11 @@ public:
     }
   }
 
+  /**
+   * Render the pause menu on the game window.
+   * @param m_window SFML RenderWindow.
+   * @param cam Cam object for camera information.
+   */
   void render(RenderWindow &m_window, Cam &cam) {
     if (m_pause) {
       m_window.clear();
@@ -75,7 +98,11 @@ public:
     }
   }
 
+  /**
+   * Check if the game is paused.
+   * @return True if the game is paused, false otherwise.
+   */
   bool isPause() const { return m_pause; }
 };
 
-#endif /*MENUPAUSE_HPP*/
+#endif /* MENUPAUSE_HPP */
