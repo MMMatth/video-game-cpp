d0cb26e6e0093951e19036220d4f1bc5193baebd
Cisse PapaElHadjiG <papa-el-hadji-g.cisse2@etu.univ-lorraine.fr>
2023-11-27 01:15:51 +0100

    adde comments on the fpsCounter class

--- include/ui/fpsCounter.hpp
+++ include/ui/fpsCounter.hpp
@@ -9,20 +9,38 @@
 using namespace sf;
 using namespace std;
 
+/**
+ * @class FpsCounter
+ * @brief Measures and displays frames per second (FPS).
+ *
+ * This class is responsible for measuring and displaying the frames per second
+ * on the game window using SFML.
+ */
 class FpsCounter {
 private:
-  Clock m_clock;
-  Coord m_coord;
-  int m_frameCount;
-  int m_fps;
+  Clock m_clock; /**< SFML clock to measure time. */
+  Coord m_coord; /**< Coordinates to display the FPS on the window. */
+  int m_frameCount; /**< Counter for frames rendered. */
+  int m_fps; /**< Calculated frames per second. */
 
 public:
+  /**
+   * Parameterized constructor for FpsCounter.
+   * @param x X-coordinate for displaying FPS on the window.
+   * @param y Y-coordinate for displaying FPS on the window.
+   */
   FpsCounter(int x, int y)
       : m_clock(), m_coord(x, y), m_frameCount(0), m_fps(0){};
 
-  /* getters */
+  /**
+   * Get the current frames per second.
+   * @return Current frames per second.
+   */
   int getFps() const { return m_fps; }
 
+  /**
+   * Update the frames per second count.
+   */
   void update() {
     if (m_clock.getElapsedTime().asSeconds() >= 1.0f) {
       m_fps = m_frameCount;
@@ -32,6 +50,11 @@ public:
     m_frameCount++;
   }
 
+  /**
+   * Render the frames per second on the game window.
+   * @param window Reference to the SFML RenderWindow.
+   * @param cam Reference to the game camera.
+   */
   void render(RenderWindow &window, Cam &cam) {
     int x = m_coord.getX() + cam.getX() - cam.getWidth() / 2;
     int y = m_coord.getY() + cam.getY() - cam.getHeight() / 2;
