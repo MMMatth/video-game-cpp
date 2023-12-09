f304308856f00eb4bcd716996e0ac6e8033edb1b
MMMatth <mat.loup04@gmail.com>
2023-12-09 01:49:43 +0100

    i start the mini map

--- src/rendering/mapRender.cpp
+++ src/rendering/mapRender.cpp
@@ -30,17 +30,28 @@ void MapRender::render(RenderWindow &window,
 void MapRender::renderMiniMap(RenderWindow &window,
                               unordered_map<string, Sprite> sprites, int camX,
                               int camY) {
-  int miniMapScale = 4; // Change this to control the size of the mini-map
-  int miniMapX = 0;
-  int miniMapY = 0;
+  int miniMapScale = 6; // Change this to control the size of the mini-map
+  int offsetX = camX + CAM_WIDTH / 2 -
+                (m_map.get_cam_width() - m_map.get_working_area().getX()) *
+                    TILE_SIZE / miniMapScale -
+                10;
+  int offsetY = camY - CAM_HEIGHT / 2 + 10;
+  int width = (m_map.get_cam_width() - m_map.get_working_area().getX()) *
+              TILE_SIZE / miniMapScale;
+  int height = (m_map.get_cam_height() - m_map.get_working_area().getY()) *
+               TILE_SIZE / miniMapScale;
+
+  drawRectangle(offsetX, offsetY, width, height, &window, Color(0, 0, 0), 100);
 
   for (int y = m_map.get_working_area().getY(); y < m_map.get_cam_height();
        y++) {
     for (int x = m_map.get_working_area().getX(); x < m_map.get_cam_width();
          x++) {
       Tile tile = m_map.get_tile(y, x);
-      drawSprites(miniMapX + tile.getX() / miniMapScale,
-                  miniMapY + tile.getY() / miniMapScale,
+      int xtemp = x - m_map.get_working_area().getX();
+      int ytemp = y - m_map.get_working_area().getY();
+      drawSprites(offsetX + xtemp * (TILE_SIZE / miniMapScale),
+                  offsetY + ytemp * (TILE_SIZE / miniMapScale),
                   sprites[tile.getBlock()->getName()], &window,
                   TILE_SIZE / miniMapScale, TILE_SIZE / miniMapScale);
     }
